ad539bc10dd8ef6c4c16726ffdc06a2142570c4a
Quan Pham <doraemonfanclub@gmail.com>
2021-04-19 19:01:13 +0700

    Calculate image

--- src/routes/authen/builder/slide-builder/slide-interactive-editor-v2/slide-block-v2/slide-block-v2.tsx
+++ src/routes/authen/builder/slide-builder/slide-interactive-editor-v2/slide-block-v2/slide-block-v2.tsx
@@ -1,8 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { Rnd } from "react-rnd";
+import { Table } from "antd";
 import { AppDefaults, MediaType, RESOURCE_PROTOCOL } from "~/common/static-data";
 import { SlideBlockType } from "~/typings/types";
 import { fileUtils } from "~/utils/utils-files";
+
+const columns = [
+  {
+    title: "Name",
+    dataIndex: "name",
+    key: "name",
+    render: (text: string) => <a>{text}</a>,
+  },
+  {
+    title: "Age",
+    dataIndex: "age",
+    key: "age",
+  },
+  {
+    title: "Address",
+    dataIndex: "address",
+    key: "address",
+  },
+];
+
+const data = [
+  {
+    key: "1",
+    name: "John Brown",
+    age: 32,
+    address: "New York No. 1 Lake Park",
+    tags: ["nice", "developer"],
+  },
+  {
+    key: "2",
+    name: "Jim Green",
+    age: 42,
+    address: "London No. 1 Lake Park",
+    tags: ["loser"],
+  },
+  {
+    key: "3",
+    name: "Joe Black",
+    age: 32,
+    address: "Sidney No. 1 Lake Park",
+    tags: ["cool", "teacher"],
+  },
+];
 
 type SlideBlockComponentType = SlideBlockType & {
   selected?: boolean;
@@ -11,6 +55,12 @@ type SlideBlockComponentType = SlideBlockType & {
 
 export const SlideBlock: React.FC<SlideBlockComponentType> = ({ type, assetName, size }) => {
   const assetUrl = `${RESOURCE_PROTOCOL}${fileUtils.getCacheDirectory()}/${assetName}`;
+
+  const initW = AppDefaults.DEFAULT_IMAGE_SCALE * (size?.w ?? 0);
+  const initH = AppDefaults.DEFAULT_IMAGE_SCALE * (size?.h ?? 0);
+
+  const [blockW, setBlockW] = useState(initW);
+  const [blockH, setBlockH] = useState(initH);
 
   const getMainComponent = () => {
     if (type === MediaType.IMAGE) {
@@ -21,13 +71,13 @@ export const SlideBlock: React.FC<SlideBlockComponentType> = ({ type, assetName,
             const topLeftX = d.x;
             const topLeftY = d.y;
             if (!size) return;
-            const centerOriginW = size.w / 2;
-            const centerOriginH = size.h / 2;
-            console.log("Actual position: ", centerOriginW, centerOriginH);
+            const centerOriginW = topLeftX + blockW / 2;
+            const centerOriginH = topLeftY + blockH / 2;
+            console.log("Origin: ", centerOriginW, centerOriginH);
           }}
           onResizeStop={(mouseEvent, direction, element, delta) => {
-            // Code...
-            console.log(delta);
+            setBlockW(blockW + delta.width);
+            setBlockH(blockH + delta.height);
           }}
           enableResizing={{
             top: false,
@@ -69,25 +119,7 @@ export const SlideBlock: React.FC<SlideBlockComponentType> = ({ type, assetName,
             height: 200,
           }}
         >
-          <table>
-            <tbody>
-              <tr>
-                <th>Firstname</th>
-                <th>Lastname</th>
-                <th>Age</th>
-              </tr>
-              <tr>
-                <td>Jill</td>
-                <td>Smith</td>
-                <td>50</td>
-              </tr>
-              <tr>
-                <td>Eve</td>
-                <td>Jackson</td>
-                <td>94</td>
-              </tr>
-            </tbody>
-          </table>
+          <Table columns={columns} dataSource={data} />
         </Rnd>
       );
     }
