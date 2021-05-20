04674406fba8801d0012c12898823b7443988e8d
Quan Pham <doraemonfanclub@gmail.com>
2021-05-20 09:39:16 +0700

    Add css for selected quiz

--- src/routes/authen/builder/quiz-builder/quiz-list/quiz-list.tsx
+++ src/routes/authen/builder/quiz-builder/quiz-list/quiz-list.tsx
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { observer } from "mobx-react";
+import clsx from "clsx";
 import { StoreContext } from "~/mobx/store-context";
 import { formattingUtils } from "~/utils/utils-formatting";
 
@@ -8,16 +9,23 @@ import "~/routes/authen/builder/quiz-builder/quiz-list/quiz-list.scss";
 export const QuizList: React.FC = observer(() => {
   const store = useContext(StoreContext);
   const { list } = store.quizListStore;
-  const { name, instruction, setIndex } = store.quizDeckStore;
+  const { name, instruction, selectedIndex, setIndex } = store.quizDeckStore;
 
   return (
     <div className="quiz-list" tabIndex={2}>
-      <div className="quiz-cell meta-cell" onClick={() => setIndex(-1)}>
+      <div
+        className={clsx("quiz-cell", "meta-cell", selectedIndex === -1 ? "selected-cell" : "")}
+        onClick={() => setIndex(-1)}
+      >
         <strong>{formattingUtils.furiganaToJSX(name)}</strong>
         <div>{formattingUtils.furiganaToJSX(formattingUtils.trimTextTo(instruction, 20))}</div>
       </div>
       {list.map((n, idx) => (
-        <div key={n.id} className="quiz-cell" onClick={() => setIndex(idx)}>
+        <div
+          key={n.id}
+          className={clsx("quiz-cell", selectedIndex !== -1 ? "selected-cell" : "")}
+          onClick={() => setIndex(idx)}
+        >
           Quiz
         </div>
       ))}
