8e4ea8227a26c9c3306ced601c6460e0fbf020a8
Quan Pham <doraemonfanclub@gmail.com>
2021-06-15 10:30:43 +0700

    Minor CSS fix

--- src/routes/authen/settings/settings-route.tsx
+++ src/routes/authen/settings/settings-route.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
-import { Menu } from "antd";
-import { UserOutlined, UploadOutlined, SettingOutlined } from "@ant-design/icons";
+import { Menu, Layout } from "antd";
+import { UserOutlined, SettingOutlined } from "@ant-design/icons";
 import { GeneralSettings } from "~/routes/authen/settings/general/general-settings";
 import { SettingKey } from "~/typings/types";
+
+const { Content, Sider } = Layout;
 
 import "~/routes/authen/settings/settings-route.scss";
 
@@ -16,21 +18,23 @@ export const SettingsRoute: React.FC = () => {
 
   return (
     <div className="settings-route">
-      <div className="left-col">
-        <Menu theme="light" mode="inline" defaultSelectedKeys={["general"]}>
-          <Menu.Item key="auth" icon={<UserOutlined />} onClick={() => setMenuKey("auth")}>
-            Tài khoản
-          </Menu.Item>
-          <Menu.Item key="general" icon={<SettingOutlined />} onClick={() => setMenuKey("general")}>
-            Thiết lập chung
-          </Menu.Item>
-          <Menu.Item key="upload" icon={<UploadOutlined />} onClick={() => setMenuKey("upload")}>
-            Tải lên
-          </Menu.Item>
-        </Menu>
-      </div>
-
-      <div className="right-col">{getSettingLayout()}</div>
+      <Layout className="site-layout-background" style={{ padding: "24px 0" }}>
+        <Sider className="site-layout-background" width={200}>
+          <Menu mode="inline" defaultSelectedKeys={["general"]} style={{ height: "100%" }}>
+            <Menu.Item
+              key="general"
+              icon={<SettingOutlined />}
+              onClick={() => setMenuKey("general")}
+            >
+              Cài đặt chung
+            </Menu.Item>
+            <Menu.Item key="auth" icon={<UserOutlined />} onClick={() => setMenuKey("auth")}>
+              Tài khoản
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Content style={{ padding: "0 24px", minHeight: 280 }}>{getSettingLayout()}</Content>
+      </Layout>
     </div>
   );
 };
