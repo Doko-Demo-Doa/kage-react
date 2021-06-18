2cac4348375dbfd014ffcc32732a51c9a36c77c0
Quan Pham <doraemonfanclub@gmail.com>
2021-06-18 18:03:17 +0700

    Move env

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,7 @@
+import React, { useEffect } from "react";
+import { ipcRenderer } from "electron";
+
+import { ElectronEventType } from "~/common/static-data";
 import { BrowserRouter, HashRouter, Switch, Route } from "react-router-dom";
 import { LoginRoute } from "~/routes/guest/login/login-route";
 import { Builder } from "~/routes/authen/builder/builder";
@@ -18,15 +22,44 @@ const AppRouter = ({ children }: { children: React.ReactElement }): React.ReactE
 
 initializeApp();
 
-const App = (): React.ReactElement => (
-  <StoreContext.Provider value={rootStore}>
-    <AppRouter>
-      <Switch>
-        <Route exact path="/" component={Builder} />
-        <Route exact path="/login" component={LoginRoute} />
-      </Switch>
-    </AppRouter>
-  </StoreContext.Provider>
-);
+const App = (): React.ReactElement => {
+  useEffect(() => {
+    ipcRenderer.send(ElectronEventType.UPDATE_CHECK, {
+      provider: process.env.REACT_APP_UPDATE_PROVIDER,
+      repo: process.env.REACT_APP_UPDATE_REPO,
+      owner: process.env.REACT_APP_REPO_OWNER,
+      private: true,
+      token: process.env.REACT_APP_GH_TOKEN,
+    });
+
+    ipcRenderer.on(ElectronEventType.UPDATE_NOT_AVAILABLE, (event, data) => {
+      console.log("No update available", data);
+    });
+
+    ipcRenderer.on(ElectronEventType.UPDATE_AVAILABLE, (_, data) => {
+      console.log("A new update is available. Downloading now...", data);
+    });
+
+    ipcRenderer.on(ElectronEventType.UPDATE_DOWNLOADED, (_, data) => {
+      console.log("Update downloaded", data);
+    });
+
+    return () => {
+      ipcRenderer.removeAllListeners(ElectronEventType.UPDATE_AVAILABLE);
+      ipcRenderer.removeAllListeners(ElectronEventType.UPDATE_DOWNLOADED);
+    };
+  }, []);
+
+    return (
+    <StoreContext.Provider value={rootStore}>
+      <AppRouter>
+        <Switch>
+          <Route exact path="/" component={Builder} />
+          <Route exact path="/login" component={LoginRoute} />
+        </Switch>
+      </AppRouter>
+    </StoreContext.Provider>
+  );
+};
 
 export default App;
--- src/routes/authen/builder/slide-builder/slide-builder.tsx
+++ src/routes/authen/builder/slide-builder/slide-builder.tsx
@@ -1,7 +1,4 @@
-import React, { useEffect } from "react";
-import { ipcRenderer } from "electron";
-
-import { ElectronEventType } from "~/common/static-data";
+import React from "react";
 
 import { SlideBuilderToolbar } from "~/routes/authen/builder/slide-builder/slide-builder-toolbar/slide-builder-toolbar";
 import { SlideList } from "~/routes/authen/builder/slide-builder/slide-list/slide-list";
@@ -13,32 +10,7 @@ import packageMeta from "../../../../../package.json";
 import "~/routes/authen/builder/slide-builder/slide-builder.scss";
 
 export const SlideBuilder: React.FC = () => {
-  useEffect(() => {
-    ipcRenderer.send(ElectronEventType.UPDATE_CHECK, {
-      provider: process.env.REACT_APP_UPDATE_PROVIDER,
-      repo: process.env.REACT_APP_UPDATE_REPO,
-      owner: process.env.REACT_APP_REPO_OWNER,
-      private: true,
-      token: process.env.REACT_APP_GH_TOKEN,
-    });
 
-    ipcRenderer.on(ElectronEventType.UPDATE_NOT_AVAILABLE, (event, data) => {
-      console.log("Khong co update", data);
-    });
-
-    ipcRenderer.on(ElectronEventType.UPDATE_AVAILABLE, (_, data) => {
-      console.log("A new update is available. Downloading now...", data);
-    });
-
-    ipcRenderer.on(ElectronEventType.UPDATE_DOWNLOADED, (_, data) => {
-      console.log("Update downloaded", data);
-    });
-
-    return () => {
-      ipcRenderer.removeAllListeners(ElectronEventType.UPDATE_AVAILABLE);
-      ipcRenderer.removeAllListeners(ElectronEventType.UPDATE_DOWNLOADED);
-    };
-  }, []);
 
   return (
     <div className="builder slide-builder">
